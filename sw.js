b111fb1d52c9113fc0b67169a384095dbbba6f3a
dmatysiek <d.matysiak@adres.pl>
2025-10-05 22:22:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // prościutki cache offline
-const CACHE = "szablonopis-v39";
+const CACHE = "szablonopis-v50";
 const ASSETS = [
   "./",
   "./index.html",
@@ -25,6 +25,7 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
 
 
 
