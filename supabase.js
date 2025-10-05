ec103b306f8f1754697aa155484a7652aa9f47ca
dmatysiek <d.matysiak@adres.pl>
2025-10-05 16:30:39 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,15 +1,14 @@
-<!-- supabase.js -->
-<script type="module">
-  import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+// supabase.js  (CZYSTY JS MODUŁ)
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-  export const SUPABASE_URL = 'https://zgeqrtrjjmaohgsmewpz.supabase.co';
-  export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZXFydHJqam1hb2hnc21ld3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1ODkwMzQsImV4cCI6MjA3NTE2NTAzNH0.VOv3f51fRpqSzF8RxvKjvbu0Z3PnuVEw4FQ02nTzORc';
+export const SUPABASE_URL = 'https://zgeqrtrjjmaohgsmewpz.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZXFydHJqam1hb2hnc21ld3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1ODkwMzQsImV4cCI6MjA3NTE2NTAzNH0.VOv3f51fRpqSzF8RxvKjvbu0Z3PnuVEw4FQ02nTzORc';
 
-  export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true, // ważne dla magic link
-    }
-  });
-</script>
+// Klient Supabase
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true, // potrzebne dla magic link
+  },
+});
